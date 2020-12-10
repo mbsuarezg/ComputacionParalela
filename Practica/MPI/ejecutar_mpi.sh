3ace3078ecc5faa9c4c7d4d9872165465cf1e231
msrz <mbsuarezg@unal.edu.co>
2020-12-10 01:19:18 +0000

    test 3 MPI

--- Practica/MPI/ejecutar_mpi.sh
+++ Practica/MPI/ejecutar_mpi.sh
@@ -8,11 +8,11 @@ mpic++ reduction.cpp `pkg-config --cflags --libs opencv` -fopenmp -o x
 clear
 for file in $IWD/images/*
 do
-	for((i = 1; i <= 4; i = i + 1))
+	for((i = 1; i <= 4; i = i * 2))
 	do
 		for ((j = 1; j <= 4; j = j * 2))
 	    do  
-	    	mpirun -np ${i} --hostfile /home/mpiuser/mpi_hosts ./x $(basename $file) 480p_version_of_$(basename $file) ${j}
+	    	mpirun -np ${i} --hostfile mpi_hosts ./x $(basename $file) 480p_version_of_$(basename $file) ${j}
 	    done
 	done
 done
